d849eba29dc660e95a86979c778261f30a512889
alikonuk1 <alikonuk1@gmail.com>
2023-09-11 20:41:08 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::io;
+use rand::Rng;
 use reqwest::Error;
-use serde_derive::{Deserialize};
+use serde_derive::Deserialize;
+use std::io;
 
 #[derive(Deserialize)]
 struct CoinGeckoResponse {
@@ -13,16 +14,16 @@ struct CurrencyInfo {
     usd: f64,
 }
 
-struct MantleFeeEstimation {
+struct L2FeeEstimation {
     l2_gas_price: f64,
     l2_gas_used: u64,
     l1_gas_price: f64,
     l1_gas_used: u64,
 }
 
-impl MantleFeeEstimation {
+impl L2FeeEstimation {
     fn new(l2_gas_price: f64, l2_gas_used: u64, l1_gas_price: f64, l1_gas_used: u64) -> Self {
-        MantleFeeEstimation {
+        L2FeeEstimation {
             l2_gas_price,
             l2_gas_used,
             l1_gas_price,
@@ -30,81 +31,103 @@ impl MantleFeeEstimation {
         }
     }
 
-    fn calculate_l2_execution_fee(&self) -> f64 {
-        self.l2_gas_price * self.l2_gas_used as f64 / 1_000_000_000.0
-    }
-
-    fn calculate_l1_rollup_fee(&self) -> f64 {
-        self.l1_gas_price * self.l1_gas_used as f64 / 1_000_000_000.0
-    }
-
     fn calculate_total_fee_in_mnt(&self) -> f64 {
-        self.calculate_l2_execution_fee() + self.calculate_l1_rollup_fee()
+        (self.l2_gas_price * self.l2_gas_used as f64 + self.l1_gas_price * self.l1_gas_used as f64)
+            / 1_000_000_000.0
     }
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
+fn random_f64_in_range(min: f64, max: f64) -> f64 {
+    let mut rng = rand::thread_rng();
+    rng.gen_range(min..=max)
+}
+
+
+fn get_input(prompt: &str) -> Result<String, io::Error> {
     let mut input = String::new();
+    println!("{}", prompt);
+    io::stdin().read_line(&mut input)?;
+    Ok(input.trim().to_string())
+}
 
-    println!("Select chain for gas calculation:");
-    println!("1: Ethereum");
-    println!("2: Mantle");
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    let chain_selection: u8 = input.trim().parse().expect("Please select a valid option");
-    input.clear();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut input = String::new();
+    let chain_selection: u8 = get_input("Select chain for gas calculation:\n1: Ethereum\n2: Mantle")
+        .expect("Failed to read line")
+        .parse()
+        .expect("Please select a valid option");
 
-    let conversion_response: CoinGeckoResponse = reqwest::get("https://api.coingecko.com/api/v3/simple/price?ids=ethereum,mantle&vs_currencies=usd")
-        .await?
-        .json()
-        .await?;
-    
-    let eth_to_usd = conversion_response.ethereum.unwrap().usd;
-    let mnt_to_usd = conversion_response.mantle.unwrap().usd;
-/*     let eth_to_mnt_ratio = eth_to_usd / mnt_to_usd; */
+    let conversion_response: CoinGeckoResponse = reqwest::get(
+        "https://api.coingecko.com/api/v3/simple/price?ids=ethereum,mantle&vs_currencies=usd",
+    )
+    .await?
+    .json()
+    .await?;
+
+    let eth_to_usd = conversion_response.ethereum.ok_or("Ethereum price not found")?.usd;
+    let mnt_to_usd = conversion_response.mantle.ok_or("Mantle price not found")?.usd;
+
+    let days: u64 = get_input("Enter the number of days for the simulation: ")
+        .expect("Failed to read line")
+        .parse()
+        .expect("Please enter a valid number");
+
+    let min_gas_price: f64 = get_input("Enter the minimum gas price (in Gwei): ")
+        .expect("Failed to read line")
+        .parse()
+        .expect("Please enter a valid number");
+
+    let max_gas_price: f64 = get_input("Enter the maximum gas price (in Gwei): ")
+        .expect("Failed to read line")
+        .parse()
+        .expect("Please enter a valid number");
+
+    let eth_gas_used: u64 = get_input("Enter the Ethereum gas used: ")
+        .expect("Failed to read line")
+        .parse()
+        .expect("Please enter a valid number for Ethereum gas used");
 
     if chain_selection == 1 {
-        println!("Enter Ethereum gas price (in Gwei): ");
-        io::stdin().read_line(&mut input).expect("Failed to read line");
-        let eth_gas_price: f64 = input.trim().parse().expect("Please enter a valid number");
-        input.clear();
+        for _ in 0..days {
+            let gas_price = random_f64_in_range(min_gas_price, max_gas_price);
 
-        println!("Enter Ethereum gas used: ");
-        io::stdin().read_line(&mut input).expect("Failed to read line");
-        let eth_gas_used: u64 = input.trim().parse().expect("Please enter a valid number");
-
-        let eth_fee = eth_gas_price * eth_gas_used as f64 / 1_000_000_000.0;
-        let eth_fee_usd = eth_fee * eth_to_usd;
-        println!("Total Transaction Fee: {:.18} ETH", eth_fee);
-        println!("(~${:.2} USD)", eth_fee_usd);
+            let eth_fee = gas_price * eth_gas_used as f64 / 1_000_000_000.0;
+            let eth_fee_usd = eth_fee * eth_to_usd;
+            println!("Total Transaction Fee: {:.18} ETH", eth_fee);
+            println!("(~${:.2} USD)", eth_fee_usd);
+        }
     } else if chain_selection == 2 {
-        println!("Enter L2 gas price (in Gwei equivalent of MNT): ");
-        io::stdin().read_line(&mut input).expect("Failed to read line");
-        let l2_gas_price: f64 = input.trim().parse().expect("Please enter a valid number");
-        input.clear();
+        let l2_min_gas_price: f64 = get_input("Enter the minimum L2 gas price (in Gwei): ")
+        .expect("Failed to read line")
+        .parse()
+        .expect("Please enter a valid number");
 
-        println!("Enter L2 gas used: ");
-        io::stdin().read_line(&mut input).expect("Failed to read line");
-        let l2_gas_used: u64 = input.trim().parse().expect("Please enter a valid number");
-        input.clear();
+        let l2_max_gas_price: f64 = get_input("Enter the maximum L2 gas price (in Gwei): ")
+        .expect("Failed to read line")
+        .parse()
+        .expect("Please enter a valid number");
 
-        println!("Enter L1 gas price (in Gwei equivalent of MNT): ");
-        io::stdin().read_line(&mut input).expect("Failed to read line");
-        let l1_gas_price: f64 = input.trim().parse().expect("Please enter a valid number");
-        input.clear();
+        let l2_gas_used: u64 = get_input("Enter the L2 gas used: ")
+        .expect("Failed to read line")
+        .parse()
+        .expect("Please enter a valid number for L2 gas used");
 
-        println!("Enter L1 gas used: ");
-        io::stdin().read_line(&mut input).expect("Failed to read line");
-        let l1_gas_used: u64 = input.trim().parse().expect("Please enter a valid number");
-        input.clear();
-        
-        let fee_estimate = MantleFeeEstimation::new(l2_gas_price, l2_gas_used, l1_gas_price, l1_gas_used);
-        let total_fee = fee_estimate.calculate_total_fee_in_mnt();
-        let total_fee_usd = total_fee * mnt_to_usd;
-        println!("Total Transaction Fee: {:.18} MNT", total_fee);
-        println!("(~${:.2} USD)", total_fee_usd);
+        for _ in 0..days {
+            let gas_price = random_f64_in_range(min_gas_price, max_gas_price);
+
+            let l2_gas_price = random_f64_in_range(l2_min_gas_price, l2_max_gas_price);
+
+            let fee_estimate =
+                L2FeeEstimation::new(l2_gas_price, l2_gas_used, gas_price, eth_gas_used);
+            let total_fee = fee_estimate.calculate_total_fee_in_mnt();
+            let total_fee_usd = total_fee * mnt_to_usd;
+            println!("Total Transaction Fee: {:.18} MNT", total_fee);
+            println!("(~${:.2} USD)", total_fee_usd);
+        }
     } else {
         println!("Invalid selection. Please run the program again.");
+        return Ok(());
     }
 
     Ok(())
